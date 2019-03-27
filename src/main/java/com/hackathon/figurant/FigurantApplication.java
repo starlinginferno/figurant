eb1c8634f6d7b6cc5d57289ec77c4f9cdf5deff1
Lili <liliszendrei@gmail.com>
2019-03-27 20:05:13 +0100

    initialize spring project'

---
+++ src/main/java/com/hackathon/figurant/FigurantApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.figurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FigurantApplication.class, args);
+    }
+
+}
